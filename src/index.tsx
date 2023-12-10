e786046383d66d4b5feb2ef6651477574c3b9f10
fabiopasilva1 <presstencao@gmail.com>
2023-12-09 23:01:41 -0300

    Fix Error Cannot read properties of undefined (reading 'split')

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ export const Thing = () => {
   return <div>the snozzberries taste like snozzberries</div>;
 };
 
-export const GetGoogleMapUrlCoordinate = (url:string) =>{
-  const coordinates = getGoogleMapUrlCordinates(url);
+export const GetGoogleMapUrlCoordinate = () =>{
+  const coordinates = getGoogleMapUrlCordinates('https://www.google.com/maps/place/Rua+Americana,+142+-+Conj.+Hab.+Pres.+Castelo+Branco,+Carapicu%C3%ADba+-+SP,+06327-320/@-23.5348058,-46.8318442,15z/data=!4m6!3m5!1s0x94cefe2b3c6e1571:0xb2d32809856b3831!8m2!3d-23.5348058!4d-46.8215443!16s%2Fg%2F11c4c504wq?authuser=0&entry=ttu');
 
   return <span>{coordinates}</span>
 }
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { GetGoogleMapUrlCoordinate, Thing } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Thing />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe('it', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GetGoogleMapUrlCoordinate />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
