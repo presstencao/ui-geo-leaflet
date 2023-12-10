8905bf28da720b694250aad7e9fe2ca541f32f2f
fabiopasilva1 <presstencao@gmail.com>
2023-12-09 22:44:50 -0300

    Add Coordiants Helpers

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,47 @@
+//@ts-check
+
+export interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+export default function getCoordinates(lat: number, lng: number): Coordinates {
+  return {
+    lat,
+    lng,
+  };
+}
+export function getLat(coordinates: Coordinates): number {
+  return coordinates.lat;
+}
+
+export function getLng(coordinates: Coordinates): number {
+  return coordinates.lng;
+}
+export function getLatLng(coordinates: Coordinates): [number, number] {
+  return [coordinates.lat, coordinates.lng];
+}
+
+export function getLatLngString(coordinates: Coordinates): string {
+  return `${coordinates.lat},${coordinates.lng}`;
+}
+export function getLatLngStringFromArray(
+  coordinates: [number, number]
+): string {
+  return `${coordinates[0]},${coordinates[1]}`;
+}
+
+export function getLatLngStringFromString(coordinates: string): string {
+  return coordinates;
+}
+export function getGoogleMapUrlCordinates(url: string): string {
+  return url.replace(
+    '{lat},{lng}',
+    getLatLngString(
+      getCoordinates(
+        parseFloat(url.split('{lat}')[1].split(',')[0]),
+        parseFloat(url.split('{lng}')[1].split(',')[0])
+      )
+    )
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { getGoogleMapUrlCordinates } from './helpers/helpers';
+
+// Delete me
+export const Thing = () => {
+  return <div>the snozzberries taste like snozzberries</div>;
+};
+
+export const GetGoogleMapUrlCoordinate = (url:string) =>{
+  const coordinates = getGoogleMapUrlCordinates(url);
+  return <span>coordinates</span>
+}
